d316047c991cc4e22aa3841d93a28a797460411f
AlexTRH <19tam99@mail.ru>
2023-05-10 18:06:22 +0300

    all functionality was finished

---
+++ src/pages/SignIn/SignIn.tsx
@@ -0,0 +1,74 @@
+import { Box, Button, Grid, TextField } from '@mui/material';
+import React from 'react';
+import GoogleIcon from '@mui/icons-material/Google';
+import './SignIn.css';
+import { Link } from 'react-router-dom';
+import LoadingSpinner from '../../components/LoadingSpinner/LoadingSpinner';
+import useAuthWithPass from '../../hooks/useAuthWithPass';
+import useGoogleAuth from '../../hooks/useGoogleAuth';
+
+const SignIn = () => {
+  const { getEmail, getPassword, authWithPass, email, password } =
+    useAuthWithPass();
+  const { googleAuth } = useGoogleAuth();
+  return (
+    <LoadingSpinner>
+      <Box className={'sign'}>
+        <Box className={'center around'}>
+          <Grid
+            container
+            spacing={2}
+            direction={'column'}
+            justifyContent="center"
+            alignItems="center"
+            className={'signin'}
+          >
+            <Grid item xs={12}>
+              <TextField
+                id="input_email"
+                label="Email"
+                variant="outlined"
+                className={'inputText'}
+                value={email}
+                onChange={getEmail}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                id="input_password"
+                label="password"
+                variant="outlined"
+                type={'password'}
+                className={'inputText'}
+                value={password}
+                onChange={getPassword}
+              />
+            </Grid>
+            <Grid item xs={5}>
+              <Grid container spacing={2}>
+                <Grid item>
+                  <Button variant="contained" onClick={authWithPass}>
+                    Sign In
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="contained" onClick={googleAuth}>
+                    {<GoogleIcon />}
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+
+            <Grid item xs={12}>
+              <small>
+                Don&apos;t have an account?{' '}
+                <Link to={'../signup'}>Sign Up</Link>
+              </small>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </LoadingSpinner>
+  );
+};
+export default SignIn;
---
+++ src/pages/SignUp/SignUp.tsx
@@ -0,0 +1,137 @@
+import React, { useRef, useState } from 'react';
+import { TextField, Button, Box, Grid } from '@mui/material';
+import LoadingSpinner from '../../components/LoadingSpinner/LoadingSpinner';
+import { Link } from 'react-router-dom';
+import './SignUp.css';
+import GoogleIcon from '@mui/icons-material/Google';
+import useGoogleAuth from '../../hooks/useGoogleAuth';
+import useCreateUser from '../../hooks/useCreateUser';
+
+const SignUp = () => {
+  const { googleAuth } = useGoogleAuth();
+  const {
+    errorConfirmPass,
+    errorEmail,
+    errorName,
+    errorPassword,
+    getConfirmPass,
+    getName,
+    getEmail,
+    getPassword,
+    name,
+    email,
+    password,
+    confirmPass,
+    createUser,
+  } = useCreateUser();
+  return (
+    <LoadingSpinner>
+      <Box className={'sign'}>
+        <Box className={'center around'}>
+          <Grid
+            container
+            spacing={2}
+            direction={'column'}
+            justifyContent="center"
+            alignItems="center"
+            className={'signup'}
+          >
+            <Grid item xs={12}>
+              <TextField
+                error={errorName}
+                id="input_name"
+                variant="outlined"
+                color="secondary"
+                label="Name"
+                className={'inputText'}
+                onChange={getName}
+                value={name}
+                fullWidth
+                required
+                sx={{ mb: 4 }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                error={errorEmail}
+                id="input_email"
+                variant="outlined"
+                color="secondary"
+                label="Email"
+                onChange={getEmail}
+                className={'inputText'}
+                value={email}
+                type={'email'}
+                fullWidth
+                required
+                sx={{ mb: 4 }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                error={errorPassword}
+                id="input_password"
+                variant="outlined"
+                color="secondary"
+                label="Password"
+                onChange={getPassword}
+                className={'inputText'}
+                value={password}
+                type={'password'}
+                required
+                fullWidth
+                sx={{ mb: 4 }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                error={errorConfirmPass}
+                id="input_confirm_password"
+                variant="outlined"
+                color="secondary"
+                label="Password Confirmation"
+                onChange={getConfirmPass}
+                className={'inputText'}
+                type={'password'}
+                value={confirmPass}
+                required
+                fullWidth
+                sx={{ mb: 4 }}
+              />
+            </Grid>
+            <Grid item xs={5}>
+              <Grid container spacing={2}>
+                <Grid item>
+                  <Button
+                    variant="contained"
+                    type="submit"
+                    onClick={createUser}
+                  >
+                    Sign Up
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button
+                    variant="contained"
+                    type="submit"
+                    onClick={googleAuth}
+                  >
+                    {<GoogleIcon />}
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+
+            <Grid item xs={12}>
+              <small>
+                Already have an account? <Link to={'../signin'}>Sign In</Link>
+              </small>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </LoadingSpinner>
+  );
+};
+
+export default SignUp;
